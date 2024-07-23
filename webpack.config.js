8b344aed024b475ccfcef0c292b53098c708a520
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-07-23 22:03:15 +0700

    lil update

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+        {
+            test: /\.vue$/,
+            loader: 'vue-loader',
+          },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+};
