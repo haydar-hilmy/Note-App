6bccde556a1473edbd91b78bf0203fe8bd3350ef
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-07-23 20:55:35 +0700

    add: netlify

--- src/script.js
+++ src/script.js
@@ -10,7 +10,7 @@ const app = createApp({
             visibleNote: false,
             ascOrder: false,
             notes: [],
-            addNote: {
+            addNote: {  
                 id: '',
                 title: '',
                 content: '',
